e72809f1b4f8da7fec9ec15313c30cf4f501b412
Victor Andersson <vabandersson@hotmail.com>
2023-03-02 10:06:53 +0100

    img changes

--- portfolio-projekt/src/Header.jsx
+++ portfolio-projekt/src/Header.jsx
@@ -4,14 +4,13 @@ import "./Header.css";
 export default function header() {
     return (
         <div className="head-container">
-            <div className="overlay">
-                <nav className="navbar-container">
-                    <h4 className="navbar-text">Home</h4>
-                    <h4 className="navbar-text">Projects</h4>
-                    <h4 className="navbar-text">Competence</h4>
-                    <h4 className="navbar-text">Contact Me</h4>
-                </nav>
-            </div>
+            <div className="overlay"></div>
+            <nav className="navbar-container">
+                <h4 className="navbar-text">Home</h4>
+                <h4 className="navbar-text">Projects</h4>
+                <h4 className="navbar-text">Competence</h4>
+                <h4 className="navbar-text">Contact Me</h4>
+            </nav>
         </div>
     );
 }
